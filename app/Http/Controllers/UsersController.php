7f4a79057cd97d1ff04a6e576809f14fbf579147
uruka <yassaho12@gmail.co.jp>
2020-08-12 10:47:23 +0000

    post

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -22,8 +22,13 @@ class UsersController extends Controller
     {
             $user=User::findOrFail($id);
             
+            $user->loadRelationshipCounts();
+            
+            $microposts=$user->microposts()->orderBy('created_at', 'desc')->paginate(10);
+            
             return view('users.show', [
                 'user' => $user,
+                'microposts'=> $microposts
                 ]);
     }
 }
